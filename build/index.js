b9cb16e8ec0a6882c67971c4d9f4cfe187759c10
Gatsu35 <arnaudgs@gmail.com>
2012-10-29 07:55:30 +0100

    create documentation

--- build/index.js
+++ build/index.js
@@ -3,7 +3,7 @@
 /*jshint node: true */
 "use strict";
 
-var buildDir = process.cwd().replace(/\/build$/,'')+'/build/';
+var buildDir = process.cwd().replace(/\/build$/, '') + '/build/';
 
 var COMPONENTS_LIST = "../components-list.json";
 
@@ -15,33 +15,61 @@ var Handlerbars = require('handlebars');
 var params = componentsListFile.parameters;
 var compList = componentsListFile.components;
 
-var componentPageLayout = fs.readFileSync(buildDir + 'tools/component_doc_template.handlebars');
+var componentPageLayoutTemplate = fs.readFileSync(buildDir + '../library/component_doc_template.handlebars', 'utf8');
 
 // iterate  components list
 var template;
-compList.forEach(function (compObject) {
+var allComponentsDocumentation = compList.map(function (compObject) {
     var name = compObject.name;
 
     // generate the differents html skins of the component
     var skinTemplate = fs.readFileSync(buildDir + '../' + compObject.path + '/' + params.componentHandlebarsName.replace('{name}', name), 'utf8');
     template = Handlerbars.compile(skinTemplate);
+
     // iterate each skins of one component
     var skinsHTML = compObject.skins.map(function (skin) {
         return template(skin);
     });
-
-    // generate the component documentation
-    // get the template
+    // get the component template
     var compTemplate = fs.readFileSync(buildDir + '../' + compObject.path + '/' + params.componentDocName.replace('{name}', name), 'utf8');
+
     var docsTemplate = Handlerbars.compile(compTemplate)({
         name:compObject.name,
         skins:skinsHTML
     });
+    // generate the component documentation
+    // get the template
+    // component file name
+    var fileName = params.componentDocName.replace('{name}', name);
 
-    console.log(docsTemplate);
 
+    // generate the html of the component documentation
+
+    var componentDocHTML = Handlerbars.compile(componentPageLayoutTemplate)({
+        name:compObject.name,
+        content:docsTemplate
+    });
+
+    //write the documentation file
+    var fileNameHTML = fileName.replace(/\.(handlebars|hbs)$/, '.html');
+    var filePath = buildDir + '../' + compObject.path + '/' + fileNameHTML;
+
+
+    console.log('Write Component documentation : ', compObject.name); //, 'in file : ', filePath);
+    var file = fs.writeFileSync(filePath, componentDocHTML);
+
+    return docsTemplate;
 });
 
+
+// get library index file
+var libraryHTML = Handlerbars.compile(fs.readFileSync(buildDir + '../library/index.handlebars', 'utf8'))({
+    components:allComponentsDocumentation
+});
+
+var libraryFile = buildDir + '../library/index.html';
+fs.writeFileSync(libraryFile, libraryHTML, 'utf8');
+console.log('Write Library File');
 
 
 /*
